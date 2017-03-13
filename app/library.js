03bee4334126bf55a2dd59f622fc2973dfd3454f
Nosisky <nosisky@gmail.com>
2017-03-13 12:34:39 +0100

    Checked if the input type is an array

--- app/library.js
+++ app/library.js
@@ -9,6 +9,12 @@ const data = {
 		else if(typeof(data) === "function") {
 			return data(true);
 		}
+		
+		//Check if input data is an Array
+		else if(Array.isArray(data)) {
+			const output = data[2];
+			return output;
+		}
 		//Check if input data is a boolean
 		else if(typeof(data) === "boolean") {
 			return data;
