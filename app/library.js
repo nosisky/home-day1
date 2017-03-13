029a84cbebe86bc219478a9445bbd1a849d6cf27
Nosisky <nosisky@gmail.com>
2017-03-13 12:36:01 +0100

    Added a new function to check if input type is a number

--- app/library.js
+++ app/library.js
@@ -9,10 +9,15 @@ const data = {
 		else if(typeof(data) === "function") {
 			return data(true);
 		}
-		
+
 		//Check if input data is an Array
 		else if(Array.isArray(data)) {
 			const output = data[2];
+			return output;
+		}
+		//Check if input data is a number
+		else if(typeof(data) === "number") {
+			const output = num.func(data);
 			return output;
 		}
 		//Check if input data is a boolean
